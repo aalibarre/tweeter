70577227f399f5e3d3867ffaf0bc6e11b36c3dd5
vagrant <aalibarre@gmail.com>
2021-11-18 04:28:28 +0000

    was able to refresh page and sorted the tweets based on most recent

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,34 @@
+
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
 
+  const renderTweets = function (tweets) {
+    let $borderTweets = $(".borderTweets");
+    // loops through tweets
+    $borderTweets.empty();
+    const newTweet = tweets.sort((a, b) => {
+        return b.created_at - a.created_at;
+    }) 
+    $.each(newTweet, (x) => {
+      const $tweetsX = createTweetElement(tweets[x]);
 
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     }, 
-//     {
-//         "user": {
-//           "name": "Descartes",
-//           "avatars": "https://i.imgur.com/nlhLi3I.png",
-//           "handle": "@rd" },
-//         "content": {
-//           "text": "Je pense , donc je suis"
-//         },
-//         "created_at": 1461113959088
-//       }
-//     ]
+      //console.log("tweestX", tweets[x]);
+      //console.log("tweestX", $tweetsX);
+      $borderTweets.append($tweetsX);
+    });
+    // takes return value and appends it to the tweets container
+    //console.log($borderTweets);
+    return $borderTweets;
+  };
 
-    const renderTweets = function(tweets) {
-       let $borderTweets = $(".borderTweets")
-        // loops through tweets
-        $borderTweets.empty();
-        $.each(tweets, (x) => {
-            const $tweetsX = createTweetElement(tweets[x])
-
-            //console.log("tweestX", tweets[x]);
-            //console.log("tweestX", $tweetsX);
-            $borderTweets.append($tweetsX)
-        })
-        // takes return value and appends it to the tweets container
-        //console.log($borderTweets);
-        return $borderTweets;
-      }
-
-    
-      const createTweetElement = function(tweet) {
-        let $tweet = $(`
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`
         <div class="tweet-container">
             <header class="avatar-box">
                 <section class="head-of-tweet">
@@ -71,43 +49,43 @@ $(document).ready(function() {
         </section>
         </footer>
         </div>
-        `)
-       
-        return $tweet;
-      }
-  
-     
+        `);
 
-      $("form").on("submit", function (evt) { 
-        evt.preventDefault();
-        const val = $(this).serialize();
-        console.log(val);
-        if (val === "text=") {
-            alert("Empty text area");
-        } else if (val.length > 140) {
-            alert("To many charachters");
-        } else {
-            $.post('/tweets', val).then((result) => {
-                console.log("result", result);
-                 })
-                 .catch(err => {
-                    console.log('ajax error caught');
-                    console.log(err); // related error
-                  });
-        }
-      
-      })
+    return $tweet;
+  };
 
-      const loadTweets = function () {
-        $.get('/tweets').then((result) => {
-        console.log("result", result);
-        renderTweets(result) 
+  $("form").on("submit", function (evt) {
+    evt.preventDefault();
+    const val = $(this).serialize();
+    console.log(val);
+    if (val === "text=") {
+      alert("Empty text area");
+    } else if (val.length > 140) {
+      alert("To many charachters");
+    } else {
+      $.post("/tweets", val)
+        .then((result) => {
+          console.log("result", result);
+          $("#tweet-text").val("")
+          loadTweets()
         })
-       .catch(err => {
-         console.log('ajax error caught');
-         console.log(err); // related error
+        .catch((err) => {
+          console.log("ajax error caught");
+          console.log(err); // related error
         });
-       }
-       loadTweets()
-    })
-      
+    }
+  });
+
+  const loadTweets = function () {
+    $.get("/tweets")
+      .then((result) => {
+        console.log("result", result);
+        renderTweets(result);
+      })
+      .catch((err) => {
+        console.log("ajax error caught");
+        console.log(err); // related error
+      });
+  };
+  loadTweets();
+});
